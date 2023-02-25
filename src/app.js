c873dbfda36770149191cdbe15c11543c1b15b11
Marie <marievstewart@gmail.com>
2023-02-25 01:53:05 +0000

    Javascript project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,50 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function changeCard() {
+  const suite = ["♦", "♥", "♠", "♣"];
+  const cards = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "Q",
+    "A"
+  ];
 
+  const cardsIndex = Math.floor(Math.random() * cards.length);
+  const cardsValue = cards[cardsIndex];
+  const center = document.querySelector("#number");
+  center.innerText = cardsValue;
+
+  const suiteIndex = Math.floor(Math.random() * suite.length);
+  const suiteValue = suite[suiteIndex];
+
+  const topSuite = document.querySelector("#suite");
+  const bottomSuite = document.querySelector("#suite2");
+
+  topSuite.innerText = suiteValue;
+  bottomSuite.innerText = suiteValue;
+
+  if (suiteIndex <= 1) {
+    topSuite.style.color = "red";
+    bottomSuite.style.color = "red";
+  } else {
+    topSuite.style.color = "black";
+    bottomSuite.style.color = "black";
+  }
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  changeCard();
+  const btn = document.querySelector("#btn");
+  btn.addEventListener("click", () => {
+    changeCard();
+  });
 };
